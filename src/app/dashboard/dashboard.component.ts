8719bd495d3f860fb6f75378aa7d72156b08b81d
unknown <sharonsheby96@gmail.com>
2023-02-09 08:57:25 +0530

    invalid message

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { DataService } from '../services/data.service';
 
 @Component({
@@ -10,38 +11,54 @@ export class DashboardComponent {
 
   user:any
 
-  acno:any
-  psw:any
-  amnt:any
-
-  acno1:any
-  psw1:any
-  amnt1:any
-
-  constructor(private ds:DataService){
+  constructor(private ds:DataService,private fb:FormBuilder){
+    
     this.user=this.ds.currentUser
+    
   }
+  depositForm=this.fb.group({
+    acno:['',[Validators.required,Validators.pattern('[0-9]+')]],
+    amnt:['',[Validators.required,Validators.pattern('[0-9]+')]],
+    psw:['',[Validators.required,Validators.pattern('[0-9a-zA-Z]+')]]
+      
+  })
+ withdrawForm=this.fb.group({
+    acno1:['',[Validators.required,Validators.pattern('[0-9]+')]],
+    amnt1:['',[Validators.required,Validators.pattern('[0-9]+')]],
+    psw1:['',[Validators.required,Validators.pattern('[0-9a-zA-Z]+')]]
+      
+  })
   deposit(){
-    var acno=this.acno
-    var psw=this.psw
-    var amnt=this.amnt
+    var acno=this.depositForm.value.acno
+    var psw=this.depositForm.value.psw
+    var amnt=this.depositForm.value.amnt
     const result=this.ds.deposit(acno,psw,amnt)
-    if(result){
-      alert(`Your ac has been credited with amount ${amnt} and the current balance is ${result}`)
+    if(this.depositForm.valid){
+        if(result){
+          alert(`Your ac has been credited with amount ${amnt} and the current balance is ${result}`)
 
-    }
-    else{
-      alert("incorrect acno or password")
-    }
+        }
+        else{
+          alert("incorrect acno or password")
+        }
+      }
+      else{
+        alert('Invalid form')
+      }
 
   }
   withdraw(){
-    var acno=this.acno1
-    var psw=this.psw1
-    var amnt=this.amnt1
+    var acno=this.withdrawForm.value.acno1
+    var psw=this.withdrawForm.value.psw1
+    var amnt=this.withdrawForm.value.amnt1
     const result = this.ds.withdraw(acno,psw,amnt)
-    if(result){
-      alert(`Your ac has been debited with amount ${amnt} and the current balance is ${result}`)
+    if(this.withdrawForm.valid){
+        if(result){
+          alert(`Your ac has been debited with amount ${amnt} and the current balance is ${result}`)
+        }
+    }
+    else{
+      alert('Invalid Form')
     }
 
   }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { DataService } from '../services/data.service';
 
@@ -11,11 +12,12 @@ export class LoginComponent{
   data="your perfect banking partner"
   inputplaceholder="Account number"
   //  or  acno:any
-  acno=''
-  psw=''
-
  
-constructor(private router:Router,private ds:DataService){ }
+constructor(private router:Router,private ds:DataService,private fb:FormBuilder){ }
+loginForm=this.fb.group({
+  acno:['',[Validators.required,Validators.pattern('[0-9]+')]],
+  psw:['',[Validators.required,Validators.pattern('[0-9a-zA-Z]+')]]
+})
 // login(a:any, b:any){
 //   // console.log(a.value)
 //   var acnum=a.value
@@ -35,16 +37,21 @@ constructor(private router:Router,private ds:DataService){ }
 //   }
 // }
 login(){
-  var acnum=this.acno
-  var psw=this.psw
+  var acnum=this.loginForm.value.acno
+  var psw=this.loginForm.value.psw
   const result=this.ds.login(acnum,psw)
-  if(result){
-    alert('login success')
-    this.router.navigateByUrl('dashboard')
-  }
-  else{
-    alert("incorrect acno or password")
-  }
+  if(this.loginForm.valid){
+     if(result){
+      alert('login success')
+      this.router.navigateByUrl('dashboard')
+      }
+      else{
+        alert("incorrect acno or password")
+      }
+    }
+    else{
+      alert('Invalid form')
+    }
 }
   // var userDetails=this.userDetails
  
