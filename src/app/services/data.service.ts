35688c4f76aa6be712e7f49169b7df2376c4fa74
unknown <sharonsheby96@gmail.com>
2023-02-10 08:59:28 +0530

    database message

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -6,22 +6,50 @@ import { Injectable } from '@angular/core';
 export class DataService {
   currentUser:any
   currentAcno:any
+  userDetails:any
 
-  constructor() { }
+  constructor() {
+    this.getData()
+   }
 
-  userDetails:any={
-    1000:{acno:1000, username:"anu", password:"anu123", balance:0, transaction: []},
-    1001:{acno:1001, username:"amal", password:"amal123", balance:0, transaction: []},
-    1002:{acno:1002, username:"angel", password:"angel123", balance:0, transaction: []},
-    1003:{acno:1003, username:"aneena", password:"aneena123", balance:0, transaction: []},
-    1004:{acno:1003, username:"exel", password:"exel123", balance:0, transaction: []}
+  // userDetails:any={
+  //   1000:{acno:1000, username:"anu", password:"anu123", balance:0, transaction: []},
+  //   1001:{acno:1001, username:"amal", password:"amal123", balance:0, transaction: []},
+  //   1002:{acno:1002, username:"angel", password:"angel123", balance:0, transaction: []},
+  //   1003:{acno:1003, username:"aneena", password:"aneena123", balance:0, transaction: []},
+  //   1004:{acno:1003, username:"exel", password:"exel123", balance:0, transaction: []}
+  // }
+  saveData(){
+    if(this.userDetails){
+      localStorage.setItem("database",JSON.stringify(this.userDetails))
+    }
+    if(this.currentUser){
+      localStorage.setItem("currentUser",this.currentUser)
+    }
+    if(this.currentAcno){
+      localStorage.setItem("currentAcno",JSON.stringify(this.currentAcno))
+    }
   }
+
+  getData(){
+    if(localStorage.getItem('database')){
+      this.userDetails=JSON.parse(localStorage.getItem('database') || "")
+    }
+    if(localStorage.getItem('currentUser')){
+      this.currentUser=localStorage.getItem('currentUser')
+    }
+    if(localStorage.getItem('currentAcno')){
+      this.currentAcno=JSON.parse(localStorage.getItem('currentAcno') || "")
+    }
+  }
+
   register(uname:any,acnum:any,psw:any){
     if(acnum in this.userDetails){
       return false
     }
     else{
-      this.userDetails[acnum]={acno:acnum,username:uname,password:psw,balance:0}
+      this.userDetails[acnum]={acno:acnum,username:uname,password:psw,balance:0,transaction: []}
+      this.saveData()
       return true
     }
   }
@@ -33,6 +61,7 @@ export class DataService {
         this.currentUser=userDetails[acnum]["username"]
         // console.log(this.currentUser);
         this.currentAcno= acnum
+        this.saveData()
 
         
         return true
@@ -59,8 +88,9 @@ export class DataService {
           // console.log(userDetails);
 
           // transaction data store
-          userDetails[acnum]["transaction"].push({Type:"Credit",amount:amnt})
+          userDetails[acnum]["transaction"].push({Type:"Credit",amount:amnt})//update
           
+          this.saveData()
 
           // return current balance
           return userDetails[acnum]["balance"]
@@ -87,8 +117,8 @@ export class DataService {
 
              // transaction data store
           userDetails[acnum]["transaction"].push({Type:"Debit",amount:amnt})
-          console.log(userDetails);
-          
+          // console.log(userDetails);
+          this.saveData()
 
           // return current balance
 
